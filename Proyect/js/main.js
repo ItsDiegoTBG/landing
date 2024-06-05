73e62095b5eca86fbfc55ee541f67b368fd2b29a
Jonthz <>
2024-06-05 10:58:20 -0500

    post

--- Proyect/js/main.js
+++ Proyect/js/main.js
@@ -11,3 +11,27 @@ let loaded = ( eventLoaded ) => {
   }
 
   window.addEventListener("DOMContentLoaded", loaded);
+
+const form = document.getElementById('formulario');
+form.addEventListener('submit', (event) => {
+event.preventDefault();
+const nombre = document.getElementById('nombre').value;
+const email = document.getElementById('email').value;
+
+const datos = {
+nombre: nombre,
+email: email,
+};
+fetch('https://prueba-f6141-default-rtdb.firebaseio.com/collection.json', {
+method: 'POST',
+body: JSON.stringify(datos),
+headers: {
+'Content-Type': 'application/json'
+}
+})
+.then(respuesta => respuesta.json())
+.then(datos => {
+console.log(datos); 
+})
+.catch(error => console.error(error));
+});
