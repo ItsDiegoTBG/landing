748aaee67bf51209156bf3c79a1d4e850e17b999
abc <>
2024-06-12 09:57:57 -0500

    More Images

--- Proyect/js/main.js
+++ Proyect/js/main.js
@@ -1,47 +1,47 @@
-let loaded = ( eventLoaded ) => {
-  
-    window.alert("landing page loaded");
-    console.log( eventLoaded );
-    submitValues();
-  
-  }
+let loaded = (eventLoaded) => {
+
+  window.alert("landing page loaded");
+  console.log(eventLoaded);
+  submitValues();
+
+}
 
 window.addEventListener("DOMContentLoaded", loaded);
 
-let submitValues = () =>{
+let submitValues = () => {
 
-const form = document.getElementById('formulario');
-form.addEventListener('submit', (event) => {
-event.preventDefault();
-const nombre = document.getElementById('nombre').value;
-const email = document.getElementById('email').value;
-const blog = document.getElementById('blog').value;
+  const form = document.getElementById('formulario');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    const blog = document.getElementById('blog').value;
 
-const datos = {
-nombre: nombre,
-email: email,
-blog: blog,
-};
-fetch('https://prueba-f6141-default-rtdb.firebaseio.com/collection.json', {
-method: 'POST',
-body: JSON.stringify(datos),
-headers: {
-'Content-Type': 'application/json'
-}
-})
-.then(respuesta => respuesta.json())
-.then(datos => {
-console.log(datos); 
-})
-.catch(error => console.error(error));
-});
+    const datos = {
+      nombre: nombre,
+      email: email,
+      blog: blog,
+    };
+    fetch('https://prueba-f6141-default-rtdb.firebaseio.com/collection.json', {
+      method: 'POST',
+      body: JSON.stringify(datos),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(respuesta => respuesta.json())
+      .then(datos => {
+        console.log(datos);
+      })
+      .catch(error => console.error(error));
+  });
 
 }
 
 let map = new Map();
 
 async function obtenerDatos() {
-  const url = "'https://prueba-f6141-default-rtdb.firebaseio.com/collection.json'"; 
+  const url = "'https://prueba-f6141-default-rtdb.firebaseio.com/collection.json'";
   const respuesta = await fetch(url);
 
   if (!respuesta.ok) {
